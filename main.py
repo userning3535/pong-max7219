96492d18545c2c3d0da24af0928a8d714f735039
Ning Xia <ning.xia@axnzone.com>
2025-06-18 09:57:05 +0200

    update display

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep
 import numpy as np
 from luma.core.interface.serial import noop, spi
 from luma.led_matrix.device import max7219
+from PIL import Image
 
 from src.element import AIPaddle, Ball, Paddle
 
@@ -12,6 +13,7 @@ def main():
     FPS = 60
     WIDTH = 16
     HEIGHT = 8
+    frame_count = 0
 
     ball = Ball(WIDTH // 2 - 1, HEIGHT // 2 - 1, 2 / FPS, 2 / FPS)
     paddle_0 = AIPaddle(0, HEIGHT // 2, 15 / FPS, screen_height=HEIGHT)
@@ -22,7 +24,7 @@ def main():
         ball.draw(matrix)
         paddle_0.draw(matrix)
         paddle_x.draw(matrix)
-        print(matrix.T)
+        matrix_display(None, matrix.T)
 
         # Update
         ball.update(WIDTH, HEIGHT, paddle_0=paddle_0, paddle_x=paddle_x)
@@ -30,11 +32,33 @@ def main():
         paddle_x.update(ball)
         sleep(1 / FPS)
 
+
 def init():
     # Setup
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=2, block_orientation=90, rotate=0)
     device.contrast(5)
+
+
+def matrix_display(device, matrix):
+    """Display a 2D numpy array on the LED matrix.
+    This is a wrapper function that converts a 2D numpy
+    matrix into a 1-bit B/W image and then displays it
+    on the LED matrix.
+
+    Parameters
+    ----------
+    device : luma.led_matrix.device.max7219
+        A Luma LED matrix device object.
+    matrix: uint8 numpy.ndarray
+        A 2D binary numpy array.
+
+    """
+    matrix_g = np.where(matrix == 1, 255, matrix)
+    image_g = Image.fromarray(matrix_g, mode="L")
+    image_bw = image_g.convert("1")
+    device.display(image_bw)
+
 
 if __name__ == "__main__":
     main()
